b2ba468534ec0d386991564686ac13d88ae725ab
Vladislav <your_email@whatever.com>
2022-09-02 20:35:17 +0300

    Add uels and views

---
+++ yatube/posts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse('Главная страница')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Палата №{slug}')
