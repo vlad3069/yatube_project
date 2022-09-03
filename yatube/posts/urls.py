28406de9b48885e6f6cfb5ecb158a2ae16f990bb
Vladislav <your_email@whatever.com>
2022-09-03 13:39:23 +0300

    Add html and static

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
+
 urlpatterns = [
     # Главная страница
-    path('', views.index),
+    path('', views.index, name='index'),
+    #Все Группы
+    path('group_list/', views.group_list, name='group_list'),
     # Группы
-    path('group/<slug:slug>/', views.group_posts),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts'),
+
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,26 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = "Это главная страница проекта Yatube"
+    text = "Последние обновления на сайте"
+    context = {
+        'title':title,
+        'text':text,
+    }
+    return render(request, template, context) 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Палата №{slug}')
+    template = 'posts/group_list.html'
+    title = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'title':title,
+        'slug':slug,
+    }
+    return render(request, template, context) 
+
+def group_list(request):
+    template = 'posts/group_list.html'
+    return render(request, template) 
