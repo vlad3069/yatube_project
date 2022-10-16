8a0f7ad9d199998c0d8a40013ea60d83d0736ea6
Vladislav <your_email@whatever.com>
2022-10-16 20:57:30 +0300

    Спринт 6 тесты пройдены

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 
 handler404 = 'core.views.page_not_found'
 handler400 = 'core.views.csrf_failure'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
